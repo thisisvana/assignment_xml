0f46ecbe64abcf18f9d77d4d22b5f22fae1a24cc
thisisvana <thisisvana@gmail.com>
2017-04-13 18:21:05 -0700

    styling

--- assignment_xml/schedule.php
+++ assignment_xml/schedule.php
@@ -7,7 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
          <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,300i,400,400i,500,500i,700" rel="stylesheet">
         <!-- <link href="css/foundation.css" rel="stylesheet" type="text/css"/> -->
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.1/css/foundation.min.css">
+        <link href="http://cdn.foundation5.zurb.com/foundation.css">
         <!-- <link rel="stylesheet" href="css/main.css"> -->
         <!-- <script src="js/vendor/modernizr-2.8.3.min.js"></script> -->
         <script src="https://use.fontawesome.com/e38b461000.js"></script>
@@ -22,6 +22,7 @@
 				padding: 2rem 1rem;
 				background-color: #4d4d4d;
 				color: #f6f6f6;
+        opacity: 0.8;
 			}
 			td {
 				padding:0.5rem 2rem;
@@ -36,35 +37,52 @@
 			}
       textarea {
         width: 20rem;
-        height: 10rem;
+        height: 8rem;
+        border: 0.1rem solid #4d4d4d;
       }
-      form {
+      form, .wform {
         background-color: #f6f6f6;
+        opacity: 0.9;
         margin-top: 2rem;
         display: flex;
         align-items: center;
         justify-content: center;
         flex-direction: column;
+        padding: 2rem;
+        width: 50%;
+        margin: auto;
+
       }
       input, select, button {
         width: 20rem;
         height: 3rem;
+        border: 0.1rem solid #4d4d4d;
       }
       li {
         list-style: none;
+      }
+      .bg {
+        background: url('img/vancity.jpg');
+        background-size: cover;
+      }
+      .wform {
+        background-color: #f6f6f6;
+				color: #4d4d4d;
+        opacity: 0.9;
       }
 
 		</style>
 
 	</head>
-	<body onLoad="sendXmlRequest('classes.xml')">
+	<body class='bg' onLoad="sendXmlRequest('classes.xml')">
 
     <div class="schedule">
   		<h2>Web Dev 11 - Term 3</h2>
-      <div class="table-container large-8 column">
+      <div class="table-container large-8">
         <table id="course_tb"></table>
 
       </div>
+    </div>
       <div class="form-container">
         <form action="data.php" method="post">
           <label for="class-field">Class</label>
@@ -88,7 +106,7 @@
           <button type="submit" id="submit" name="submit">Add</button>
         </form>
       </div>
-      </div>
+
 
     <?php
 
@@ -125,7 +143,7 @@
 
 
 
-    <div>
+    <div class="wform">
     <h3>Check the weather in your city</h3>
     <form id="weather-form" action="#" method="post">
       <label for="city-select">Select City</label>
@@ -136,14 +154,10 @@
       </select>
 
     </form>
-
-
-
-    </div>
     <div id="weatherC">
 
     </div>
-
+    </div>
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 		<script src="data.js"></script>
